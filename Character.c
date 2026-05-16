4fc85683d9c564e523fc5ca3f1f1f5f775de5dc4
Md. Shakibul Hossan Mozomder <158428314+Shakib-ul@users.noreply.github.com>
2026-05-16 11:03:57 +0600

    Add character input/output program

---
+++ Character.c
@@ -0,0 +1,15 @@
+// Take a character as input and display it as output.
+
+#include<stdio.h>
+
+int main()
+{
+ char ch;
+ 
+ printf("Enter a character: ");
+ scanf("%c", &ch);
+ 
+ printf("You entered: %c", ch);
+ 
+ return 0;
+}
