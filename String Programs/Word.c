e75f5880f484e301498cb736ff28f04f6ca0173e
Md. Shakibul Hossan Mozomder <158428314+Shakib-ul@users.noreply.github.com>
2026-05-18 05:24:09 +0600

    Add word input/output program

---
+++ String Programs/Word.c
@@ -0,0 +1,15 @@
+// Take a word as input and display it as output.
+
+#include<stdio.h>
+
+int main()
+{
+ char wrd[50];
+
+ printf("Enter a word: ");
+ scanf("%s", wrd);
+ 
+ printf("You entered: %s", wrd);
+ 
+ return 0;
+}
